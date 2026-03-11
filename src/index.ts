1fe1a0d7f3940390bcba3e6a02478871b10d53bb
Srikar Gadagoju <you@example.com>
2026-03-11 12:06:41 +0530

    signin endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
 import express from "express"; 
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { UserModel } from "./db";
 
 const app=express();
+app.use(express.json());
 
-app.get
+app.post("/api/v1/signin",async (req,res) =>{
+    const username=req.body.username;
+    const password=req.body.password;
+    await UserModel.create({
+        username:username,
+        password: password
+    })
+    res.json({
+        message:"User signed up"
+    })
+
+
+    
+
+
+})
+
+app.post("/api/v1/signup", (req,res) =>{
+    
+})
+app.post("/api/v1/content", (req,res) =>{
+    
+})
+app.get("/api/v1/content", (req,res) =>{
+    
+})
+
+app.delete("/api/v1/content", (req,res) =>{
+    
+})
+app.post("/api/v1/brain/share", (req,res) =>{
+    
+})
+app.get("/api/v1/brain/shareLink", (req,res) =>{
+    
+})
